2322e81d17ab3c71eb3b72d3c11cff59370f359a
Jordan Harband <ljharb@gmail.com>
2026-01-06 10:04:37 -0800

    [patch] remove useless ESM entrypoint

--- index.mjs
+++
@@ -1,9 +0,0 @@
-import callBind from 'call-bind';
-
-import getPolyfill from 'number.isfinite/polyfill';
-
-export default callBind(getPolyfill(), Number);
-
-export { default as getPolyfill } from 'number.isfinite/polyfill';
-export { default as implementation } from 'number.isfinite/implementation';
-export { default as shim } from 'number.isfinite/shim';
